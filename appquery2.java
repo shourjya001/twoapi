569fb05253949596cb2e6de6a07ee2a47651b9a3
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2025-06-01 23:07:52 +0530

    Update appquery2.java

--- appquery2.java
+++ appquery2.java
@@ -1,12 +1,14 @@
-// Separate AppQueries.java file - Add these new entries to your existing AppQueries enum
+// AppQueries.java - Separate enum file
+package com.socgen.riskweb.enums; // or whatever package you use for enums
+
 public enum AppQueries {
-    // Your existing queries
+    // Existing queries
     QRY_ACTIONCODE_TRUNCATE(new StringBuffer("TRUNCATE TABLE \"MAESNUMIPL\"").toString()),
     QRY_PRIMARYROLE_TRUNCATE(new StringBuffer("TRUNCATE TABLE \"TMAESNUMIPL\"").toString()),
     QRY_SAVE_PRIMARYROLE(new StringBuffer("INSERT INTO \"TMAESNUMIPL\" (\"entityId\",\"code\",\"subbookingId\") VALUES (?,?,?)").toString()),
-    QRY_SAVE_ACTIONCODE(new StringBuffer("INSERT INTO \"WK_MAESTRO_PRIMROLE_DBE\" (\"bdrId\",\"businessEntity\")").toString()),
+    QRY_SAVE_ACTIONCODE(new StringBuffer("INSERT INTO \"WK_MAESTRO_PRIMROLE_DBE\" (\"bdrId\",\"businessEntity\") VALUES (?,?)").toString()),
     
-    // New queries for external registration - ADD THESE TO YOUR EXISTING ENUM
+    // New queries for external registration
     QRY_EXTERNAL_TRUNCATE(new StringBuffer("TRUNCATE TABLE \"TMAESNUMIPL_EXTERNAL\"").toString()),
     QRY_SAVE_EXTERNAL(new StringBuffer("INSERT INTO \"TMAESNUMIPL_EXTERNAL\" (\"entityId\",\"code\",\"subbookingId\") VALUES (?,?,?)").toString());
 
